f233ad0d97a94a384fec444819c1cd37c9e8cdfd
srokks <srokks@gmail.com>
2021-10-22 12:32:56 +0200

    reorganizing of code

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,8 +1,21 @@
 # write your code here
 # file_name = input()
 file_name = 'test_file.py'
+def check_line_lenght(line:str):
+    if len(line) > 79:
+        return True
+def check_indentation(line):
+    pass
+def check_for_errors(line:str):
 
+    error_list = []
+    if check_line_lenght(line):
+        error_list.append({'S001':'Too long'})
+    if check_indentation(line):
+        error_list.append({'S001':'Too long'})
+    return error_list
 with open(file_name,'r') as f:
     for line_no,line in enumerate(f.readlines(),1):
-        if len(line)>79:
-            print(f"Line {line_no}: S001 Too long")
+        error_list = check_for_errors(line)
+        if len(error_list) > 0:
+            print(error_list)
