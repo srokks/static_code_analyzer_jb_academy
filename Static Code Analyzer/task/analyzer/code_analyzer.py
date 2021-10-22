3807ca006253bff9fc42ac0afec8f7adbece3044
srokks <srokks@gmail.com>
2021-10-22 12:39:21 +0200

    added skeleton for error check

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,21 +1,55 @@
 # write your code here
 # file_name = input()
 file_name = 'test_file.py'
-def check_line_lenght(line:str):
+
+
+def check_line_lenght(line: str):
     if len(line) > 79:
         return True
+
+
 def check_indentation(line):
     pass
-def check_for_errors(line:str):
 
+
+def check_spaces_before(line):
+    pass
+
+
+def check_semicol_end(line):
+    pass
+
+
+def todo_found(line):
+    pass
+
+
+def check_blank_preciding(line):
+    pass
+
+
+def check_for_errors(line: str):
     error_list = []
     if check_line_lenght(line):
-        error_list.append({'S001':'Too long'})
+        error_list.append({'S001': 'Too long'})
     if check_indentation(line):
-        error_list.append({'S001':'Too long'})
+        error_list.append({'S002': 'Indentation is not a multiple of four;'})
+    if check_semicol_end(line):
+        error_list.append({'S003': 'Unnecessary semicolon after a statement (note that semicolons are acceptable in '
+                                   'comments);'})
+    if check_spaces_before(line):
+        error_list.append({'S004': 'Less than two spaces before inline comments;'})
+    if todo_found(line):
+        error_list.append({'S005': 'TODO found (in comments only and case-insensitive);'})
+    if check_blank_preciding(line):
+        error_list.append({'S006': "More than two blank lines preceding a code line (applies to the first "
+                                   "non-empty line)."})
+
     return error_list
-with open(file_name,'r') as f:
-    for line_no,line in enumerate(f.readlines(),1):
+
+
+with open(file_name, 'r') as f:
+    for line_no, line in enumerate(f.readlines(), 1):
         error_list = check_for_errors(line)
         if len(error_list) > 0:
-            print(error_list)
+            print(f"Line {line_no}:", error_list)
